f274d215cab0df3ddc9b04e886c618ee2dea50eb
Rohit Sahu <robinkhan.sahu@gmail.com>
2019-02-03 01:18:30 +0530

    added metadata upload

--- functions/onUpload.js
+++ functions/onUpload.js
@@ -3,7 +3,8 @@ const {
 } = require('@google-cloud/storage');
 
 var admin = require("firebase-admin");
-
+const requestImageSize = require('request-image-size');
+const { getAudioDurationInSeconds } = require('get-audio-duration')
 const gcs = new Storage();
 const os = require('os');
 const path = require('path');
@@ -13,7 +14,11 @@ const db = admin.database()
 var videoScreen = require('video-screen');
 var fs = require('fs');
 var gs = require('gs');
-
+var ffmpeg = require("fluent-ffmpeg");
+var ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+var ffprobePath = require("@ffprobe-installer/ffprobe").path;
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
 exports.handler = function (object) {
     const bucket = object.bucket;
     const contentType = object.contentType;
@@ -33,9 +38,9 @@ exports.handler = function (object) {
     const tempLocalDir = path.join(os.tmpdir(), dir)
     const isVideo = contentType.startsWith('video/')
     const isPdf = contentType.startsWith('application/pdf')
-
+    const isaudio=contentType.startsWith('audio/')
     const sizes = [64, 128, 256];
-
+    console.log(object)
     if (path.basename(filePath).startsWith('Thumb')) {
         return Promise.resolve();
     } else if (isImage) {
@@ -43,7 +48,8 @@ exports.handler = function (object) {
             size => {
                 return destBucket.file(filePath).download({
                         destination: tmpFilePath
-                    }).then(() => {
+                    })
+                    .then(() => {
                         return spawn('convert', [tmpFilePath, '-thumbnail', size.toString() + "x" + size.toString(), tmpFilePath])
                     })
                     .then(() => {
@@ -67,11 +73,26 @@ exports.handler = function (object) {
                     })
             }
         )
+        var url
         return myBucket.file(filePath).getSignedUrl({
                 action: 'read',
                 expires: '03-09-2491'
-            }).then((err) => {
-                return db.ref("entries/" + dir).child('/url').set(err[0])
+            })
+            .then((res) => {
+                url = res[0]
+                return requestImageSize(url)
+            })
+            .then((size) => {
+                var meta = {
+                    size: {
+                        height: size.height,
+                        width: size.width
+                    }
+                }
+                return db.ref("entries/" + dir).set({
+                    meta: meta,
+                    url: url
+                })
             })
             .then(() => {
                 return Promise.all(uploadPromises)
@@ -119,6 +140,10 @@ exports.handler = function (object) {
                 })
             }).then(err => {
                 return db.ref("entries/" + dir).child('/Thumb').set(err[0])
+            }).then(() => {
+                return db.ref("entries/" + dir).child("/meta").set({
+                    size: object.size / 1024
+                })
             })
             .then(() => {
                 return Promise.resolve()
@@ -127,27 +152,66 @@ exports.handler = function (object) {
                 return console.log(err)
             })
     } else if (isVideo) {
-
+        var snapshotTime;
         return mkdirp(tempLocalDir)
-
-
             .then(() => {
                 return destBucket.file(filePath).download({
                     destination: tmpFilePath
                 })
             })
+            .then(() => {
+                var Meta;
+                return new Promise((resolve, reject) => {
+                    ffmpeg.ffprobe(tmpFilePath, (err, metadata) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            var minutes=metadata.format.duration/60
+                            var seconds=Math.floor((minutes%1)*60)
+                            minutes=Math.floor(minutes)
+                            Meta = {
+                                duration: minutes+":"+seconds
+                            }
+                            if (metadata.format.duration * 0.1 > 30) {
+                                snapshotTime = "30"
+                            } else if (metadata.format.duration <= 1) {
+                                snapshotTime = "0"
+                            } else {
+                                var timeat10 = Math.floor(metadata.format.duration * 0.1)
+                                if (timeat10 < 10) {
+                                    snapshotTime = "0" + timeat10
+                                } else {
+                                    snapshotTime = timeat10;
+                                }
 
+                            }
+
+                            console.log(metadata);
+                            resolve(Meta);
+
+                        }
+                    })
+                })
+            })
+            .then((Meta) => {
+                return db.ref("entries/" + dir).child("/meta").set(Meta)
+            })
             .then(() => {
                 return new Promise((resolve, reject) => {
-                 videoScreen(tmpFilePath, (err, screenshot) => {
-                    if (err) reject(err)
-                
+                    var options = {
+                        time: '00:00:' + snapshotTime,
+                        width: 300,
+                        height: 300
+                    }
+                    videoScreen(tmpFilePath, options, (err, screenshot) => {
+                        if (err) reject(err)
+
                         fs.writeFile(tempNewThumbDir, screenshot, () => {
                             resolve()
                         })
-                    
-                });
-            })
+
+                    });
+                })
             })
             .then(() => {
                 return destBucket.upload(tempNewThumbDir, {
@@ -162,6 +226,7 @@ exports.handler = function (object) {
             }).then(err => {
                 return db.ref("entries/" + dir).child('/Thumb').set(err[0])
             })
+
             .then(() => {
                 return Promise.resolve()
             })
@@ -172,5 +237,24 @@ exports.handler = function (object) {
 
 
 
+    } else if (isaudio){
+         return destBucket.file(filePath).download({
+            destination: tmpFilePath
+        }).then(()=>{
+            return getAudioDurationInSeconds(tmpFilePath)
+        }).then((duration)=>{
+            var minutes=duration/60
+            var seconds=Math.floor((minutes%1)*60)
+            minutes=Math.floor(minutes)
+            var meta = {
+                duration: minutes+":"+seconds
+            }
+            return  db.ref("entries/" + dir).child("/meta").set(meta)
+        }).then(()=>{
+            return Promise.resolve()
+        }).catch((err)=>{
+            return Promise.reject(err)
+        })
     }
+    
 }
